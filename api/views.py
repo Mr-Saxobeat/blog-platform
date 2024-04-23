10bd78085990b551678471de6a1ff73b1ccb1d18
Weiglas <weiglasribeiro@gmail.com>
2024-04-23 15:03:27 -0300

    feat: create comments views

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from api import serializers
 from django.contrib.auth.models import User
-from api.models import Post
+from api.models import Post, Comment
 from rest_framework import permissions
 from api.permissions import IsOwnerOrReadOnly
 
@@ -14,6 +14,7 @@ class PostList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = serializers.PostSerializer
@@ -21,9 +22,26 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
                           IsOwnerOrReadOnly]
 
 
+class CommentList(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
