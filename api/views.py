6d8b08af4702e042b99db26463476477d5373a75
Weiglas <weiglasribeiro@gmail.com>
2024-04-23 14:50:51 -0300

    feat: add permissions to views

--- api/views.py
+++ api/views.py
@@ -2,11 +2,14 @@ from rest_framework import generics
 from api import serializers
 from django.contrib.auth.models import User
 from api.models import Post
+from rest_framework import permissions
+from api.permissions import IsOwnerOrReadOnly
 
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -14,6 +17,8 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
 
 
 class UserList(generics.ListAPIView):
