47ae30580589f67f3d03e421902fe4230cc813b2
Weiglas <weiglasribeiro@gmail.com>
2024-04-23 15:07:35 -0300

    feat: add categories urls

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,4 @@
+from rest_framework.urlpatterns import format_suffix_patterns
 from django.urls import path
 from api import views
 
@@ -7,4 +8,10 @@ urlpatterns = [
     path('posts/', views.PostList.as_view()),
     path('posts/<int:pk>/', views.PostDetail.as_view()),
     path('comments/', views.CommentList.as_view()),
+    path('comments/<int:pk>/', views.CommentDetail.as_view()),
+    path('categories/', views.CategoryList.as_view()),
+    path('categories/<int:pk>/', views.CategoryDetail.as_view()),
 ]
+
+
+urlpatterns = format_suffix_patterns(urlpatterns)
