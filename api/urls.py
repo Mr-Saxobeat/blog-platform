9568f926b64101ea3eb4b2f2c4a7ba905344c3f6
Weiglas <weiglasribeiro@gmail.com>
2024-04-25 09:14:22 -0300

    feat: fix api views on urls

--- api/urls.py
+++ api/urls.py
@@ -5,13 +5,12 @@ from api import views
 urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
-    path('posts/', views.ListPosts.as_view()),
-    path('posts/new/', views.CreatePost.as_view()),
+    path('posts/', views.ListCreatePosts.as_view()),
     path('posts/<int:pk>/', views.DetailPost.as_view()),
-    path('comments/', views.CommentList.as_view()),
+    path('comments/', views.ListCreateComments.as_view()),
     path('comments/<int:pk>/', views.CommentDetail.as_view()),
-    path('categories/', views.CategoryList.as_view()),
-    path('categories/<int:pk>/', views.CategoryDetail.as_view()),
+    path('categories/', views.ListCreateCategory.as_view()),
+    path('categories/<int:pk>/', views.DetailCategory.as_view()),
 ]
 
 
