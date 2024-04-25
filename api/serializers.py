d9f462b1d494c6e99d177ddeaf13eb50d026135a
Weiglas <weiglasribeiro@gmail.com>
2024-04-25 09:11:56 -0300

    feat: use UserSerializer on PostSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,21 @@ from django.contrib.auth.models import User
 from api.models import Post, Comment, Category
 
 
+class UserSerializer(serializers.ModelSerializer):
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    categories = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'posts',
+            'comments',
+            'categories'
+            ]
+
 class CategorySerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
@@ -18,7 +33,7 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class PostSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
+    owner = UserSerializer(read_only=True)
     comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     categories = CategorySerializer(many=True, read_only=True)
 
@@ -44,20 +59,4 @@ class CommentSerializer(serializers.ModelSerializer):
             'body',
             'owner',
             'post'
-            ]
-
-
-class UserSerializer(serializers.ModelSerializer):
-    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    categories = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-
-    class Meta:
-        model = User
-        fields = [
-            'id',
-            'username',
-            'posts',
-            'comments',
-            'categories'
             ]
