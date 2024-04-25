7f89225a242bcc4d3fc871d200c8ac7c49c3ba01
Weiglas <weiglasribeiro@gmail.com>
2024-04-25 09:13:35 -0300

    feat: add blog_ui url as /, api url as api/

--- server/urls.py
+++ server/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
+    path('api/', include('api.urls')),
+    path('', include('blog_ui.urls')),
 ]
