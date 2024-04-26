66613f4e30d218e595bb86d514b9a57fa786903a
Weiglas <weiglasribeiro@gmail.com>
2024-04-26 15:31:42 -0300

    feat: add static urls

--- server/urls.py
+++ server/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', include('blog_ui.urls')),
+    
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
