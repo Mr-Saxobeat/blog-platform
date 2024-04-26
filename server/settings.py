5a7c4fc01b6afea04f82ae7d50b31a68a8f510dc
Weiglas <weiglasribeiro@gmail.com>
2024-04-26 15:00:20 -0300

    feat: use envs on settings

--- server/settings.py
+++ server/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', "w%fr=q(x2)e*xob#*ql59zk&fn-4r!h4l!-17-)6^xfh&ec=!k")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
